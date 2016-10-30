9493d001a846f16aae86a5f460a3dca9895265a8
autodidaddict <alothien@gmail.com>
2016-10-30 13:20:05 -0400

    Added event (past tense verb)

--- src/StatlerWaldorfCorp.LocationReporter/Models/MemberLocationRecordedEvent.cs
+++ src/StatlerWaldorfCorp.LocationReporter/Models/MemberLocationRecordedEvent.cs
@@ -2,11 +2,12 @@ using System;
 
 namespace StatlerWaldorfCorp.LocationReporter.Models
 {
-    public class LocationReport
+    public class MemberLocationRecordedEvent
     {
         public String Origin { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
         public Guid MemberID { get; set; }
+        public long RecordedTime { get; set; }
     }
 }
