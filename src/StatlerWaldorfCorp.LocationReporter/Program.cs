9e9ebeff693c4d9566abf5f6159016d096ecb6f3
autodidaddict <alothien@gmail.com>
2016-10-30 13:52:50 -0400

    Adding ASP.NET MVC and Kestrel

--- src/StatlerWaldorfCorp.LocationReporter/Program.cs
+++ src/StatlerWaldorfCorp.LocationReporter/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
 
 namespace StatlerWaldorfCorp.LocationReporter
 {
@@ -6,7 +9,19 @@ namespace StatlerWaldorfCorp.LocationReporter
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IConfiguration config = new ConfigurationBuilder()
+ 		.AddCommandLine(args)
+		.Build();
+
+	    Startup.Args = args;
+
+	    var host = new WebHostBuilder()
+		.UseKestrel()
+		.UseStartup<Startup>()
+		.UseConfiguration(config)
+		.Build();
+
+	    host.Run();
         }
     }
 }
---
+++ src/StatlerWaldorfCorp.LocationReporter/Startup.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+
+namespace StatlerWaldorfCorp.LocationReporter
+{
+    public class Startup
+    {
+        public static string[] Args { get; set; } = new String[] {};
+        private ILogger logger;
+        private ILoggerFactory loggerFactory;
+
+        public Startup(IHostingEnvironment env, ILoggerFactory loggerFactory) 
+        {
+            var builder = new ConfigurationBuilder()
+		.AddEnvironmentVariables()
+		.AddCommandLine(Startup.Args);
+	    Configuration = builder.Build();
+
+	    this.loggerFactory = loggerFactory;
+	    this.loggerFactory.AddConsole(LogLevel.Information);
+	    this.loggerFactory.AddDebug();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services) 
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app) 
+        {
+            app.UseMvc();
+        }
+    }
+}
+
