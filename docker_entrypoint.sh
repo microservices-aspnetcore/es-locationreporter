151ca8b26f63406b342477c531d8caed283f1d4f
autodidaddict <alothien@gmail.com>
2017-03-12 08:55:44 -0400

    Fixing docker entrypoint

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 cd /pipeline/source/app/publish
 echo "starting"
-mkdir /pipeline/source/app/publish/tmp
 
-export TMPDIR=/pipeline/source/app/publish/tmp
 dotnet StatlerWaldorfCorp.LocationReporter.dll --server.urls=http://0.0.0.0:${PORT-"8080"}
